e16796308a57a525c738adec7ac5e4ab37625032
Ali Ahmad <aliahmadgujjar895@gmail.com>
2025-01-13 04:31:50 +0500

    the best

--- main.c
+++ main.c
@@ -82,6 +82,12 @@ typedef struct {
     size_t buffer_length;
 } Input_Buffer;
 
+typedef struct {
+    Table*table;
+    uint32_t row_num;
+    bool end_of_table;
+
+} Cursor;
 
 // thefuncs
 
@@ -89,6 +95,23 @@ void show_error(const char *err){
     perror(err);
     exit(EXIT_FAILURE);
 }
+
+Cursor * table_start(Table*table){
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+    return cursor;
+}
+
+Cursor * table_end(Table*table){
+    Cursor *cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+    return cursor;
+}
+
 
 Pager *pager_open(const char*filename){
     int fd = open(O_RDWR | O_CREAT, S_IRWXU | S_IRUSR);
@@ -244,12 +267,20 @@ void *get_page(Pager*pager, uint32_t page_num){
     return pager->pages[page_num];
 }
 
-void *row_slot(Table *table, uint32_t row_num){
+void *cursor_value(Cursor *cursor){
+    uint32_t row_num = cursor->row_num;
     uint32_t page_num = row_num / ROWS_PER_PAGE; // giving the current working page out of TABLE_MAX_PAGES
-    void *page = get_page(table->pager, page_num);
+    void *page = get_page(cursor->table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE; // it gives us the current row
     uint32_t byte_offset = row_offset * ROW_SIZE; // it gives us the starting point of our bytes
     return page + byte_offset;
+}
+
+void cursor_advance(Cursor *cursor){
+    cursor->row_num += 1;
+    if(cursor->row_num >= cursor->table->num_rows){
+        cursor->end_of_table = true;
+    }
 }
 
 void pager_flush(Pager *pager, uint32_t page_num, uint32_t size){
@@ -316,17 +347,20 @@ ExecuteResult execute_insert(Statement *statement, Table *table){
         return EXECUTE_TABLE_FULL;
     }
     Row *row_to_insert = &(statement->row_to_insert);
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    Cursor *cursor = table_end(table);
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
     return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_select(Statement *statement, Table *table){
+    Cursor *cursor = table_start(table);
     Row row;
     if(table->num_rows > 0){
-        for(uint32_t i = 0; i < table->num_rows; i++){
-            deserialize_row(row_slot(table, i), &row);
+        while(!(cursor->end_of_table)){
+            deserialize_row(cursor_value(cursor), &row);
             print_row(&row);
+            cursor_advance(cursor);
         }
     }else{
         printf("db is empty\n");
